4b9e9de1fe2b35e06289f15b5de4764426ffc551
NicoleYue <43015979+NicoleYue@users.noreply.github.com>
2025-06-14 16:26:14 -0500

    draft

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,15 +5,27 @@ import sys
 # -- Project Information -----------------------------------------------------
 
 project = 'XLeRobot'
-author = 'Your Name or Team'
+author = 'Vector & Nicole'
 release = '0.1'
 
 # -- General Configuration ---------------------------------------------------
 
 extensions = [
-    'myst_parser',       # Enables Markdown support
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
+    "myst_parser",
+    "sphinx_subfigure",
+    "sphinxcontrib.video",
+    "sphinx_togglebutton",
+    "sphinx_design"
 ]
-
+myst_enable_extensions = ["colon_fence", "dollarmath"]
+myst_heading_anchors = 4
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -25,5 +37,6 @@ myst_enable_extensions = [
 
 # -- Options for HTML Output ------------------------------------------------
 
-html_theme = 'furo'       # Or 'sphinx_rtd_theme', 'alabaster', etc.
+#html_theme = 'furo'       # Or 'sphinx_rtd_theme', 'alabaster', etc.
+html_theme = "pydata_sphinx_theme"
 html_static_path = ['_static']
