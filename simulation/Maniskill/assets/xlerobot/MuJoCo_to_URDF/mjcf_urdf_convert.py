c3e478c0bf4a361de29a5769dea3de4d4c165aa8
Tarun Reddy Nukala <tarunreddynukala@gmail.com>
2025-09-21 18:59:34 -0500

    Update URDF conversion output filename
    Changed the output filename in mjcf_urdf_convert.py from 'xlerobot.urdf' to 'xlerobot_converted.urdf' and added the generated 'xlerobot_converted.urdf' file. This ensures consistency between the conversion script and the generated URDF asset.

--- simulation/Maniskill/assets/xlerobot/MuJoCo_to_URDF/mjcf_urdf_convert.py
+++ simulation/Maniskill/assets/xlerobot/MuJoCo_to_URDF/mjcf_urdf_convert.py
@@ -1,4 +1,4 @@
 from mjcf_urdf_simple_converter import convert
-convert("xlerobot.xml", "xlerobot.urdf")
+convert("xlerobot.xml", "xlerobot_converted.urdf")
 # or, if you are using it in your ROS package and would like for the mesh directories to be resolved correctly, set meshfile_prefix, for example:
 # convert("model.xml", "model.urdf", asset_file_prefix="package://your_package_name/model/")
